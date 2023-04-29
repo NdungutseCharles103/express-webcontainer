18f15fe79528749fa03b6466c662ea53c8424aba
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2023-04-29 15:00:11 +0200

    feat: vercel routes configurations

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
+import pages from "vite-plugin-pages";
 
 export default defineConfig({
+  root: "./src",
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
     },
   },
+  plugins: [
+    pages(),
+  ],
 });
